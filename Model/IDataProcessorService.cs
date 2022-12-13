db64e8c0e7d04ffcd0b3498d0c245ded405e4e35
Alexander Malyshkin <alexander.malyshkin@gmail.com>
2022-12-13 20:49:54 +0300

    supplied brief documentation for IDataProcessorService

--- Model/IDataProcessorService.cs
+++ Model/IDataProcessorService.cs
@@ -2,14 +2,65 @@ namespace Model;
 
 public interface IDataProcessorService
 {
+    /// <summary>
+    /// Returns all available jobs
+    /// </summary>
+    /// <returns>jobs array</returns>
     IEnumerable<DataJobDTO> GetAllDataJobs();
+    
+    /// <summary>
+    /// Retrieve jobs for a particular status
+    /// </summary>
+    /// <param name="status"></param>
+    /// <returns>IEnumerable of DataJobDTO</returns>
     IEnumerable<DataJobDTO> GetDataJobsByStatus(DataJobStatus status);
+    
+    /// <summary>
+    /// Retrieve data job by its id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     DataJobDTO GetDataJob(Guid id);
+    
+    /// <summary>
+    /// Create a new data job
+    /// </summary>
+    /// <param name="dataJob"></param>
+    /// <returns></returns>
     DataJobDTO Create(DataJobDTO dataJob);
+    
+    /// <summary>
+    /// Update an existing data job
+    /// </summary>
+    /// <param name="dataJob"></param>
+    /// <returns></returns>
     DataJobDTO Update(DataJobDTO dataJob);
+    
+    /// <summary>
+    /// Delete an existing data job
+    /// </summary>
+    /// <param name="dataJobID"></param>
     void Delete(Guid dataJobID);
+    
+    /// <summary>
+    /// Start an existing data job
+    /// </summary>
+    /// <param name="dataJobId"></param>
+    /// <returns></returns>
     bool StartBackgroundProcess(Guid dataJobId);
+    
+    /// <summary>
+    /// Retrieve the status of a data job
+    /// </summary>
+    /// <param name="dataJobId"></param>
+    /// <returns></returns>
     DataJobStatus GetBackgroundProcessStatus(Guid dataJobId);
+    
+    /// <summary>
+    /// Retrieve results of a data job
+    /// </summary>
+    /// <param name="dataJobId"></param>
+    /// <returns></returns>
     List<string> GetBackgroundProcessResults(Guid dataJobId);
 }
 
